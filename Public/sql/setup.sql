b98c581f2b48c9d7d6a6bd699e12187932ae4d4a
jcubillos2017 <jcubillos2017@outlook.cl>
2025-04-24 01:48:27 -0400

    Segunda Modificacion

--- Public/sql/setup.sql
+++
@@ -1,9 +0,0 @@
-create database coningenio;
-use coningenio;
-
-create table servicios (
-Consultoria_digital varchar (45) null,
-Soluciones_Multiexperiencia varchar (45) null,
-Evolucion_de_Ecosistemas varchar(45) null,
-Soluciones_Low_Code varchar(45) null
-);
